5db92db135223fef5b76cb8d00867652bfa4cdec
As-wm <unknow-master@hotmail.com>
2018-10-29 20:44:28 -0400

    Postcss installed

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('postcss-preset-env')({
+            preserve: false,
+        }),
+        require('postcss-custom-selectors')({
+            preserve: false,
+      }),
+        require('postcss-custom-media'),
+        require('postcss-apply'),
+        require('postcss-nesting'),
+      
+        require('postcss-font-magician'),
+        require('css-mqpacker'),
+        require('cssnano'),
+    ]
+  }
