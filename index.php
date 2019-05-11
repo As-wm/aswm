9af11eeb55928a31a3b4903835b3e7b1d249f071
coloso2050 <coloso2050@gmail.com>
2019-05-11 13:21:12 -0400

    commit de prueba desde codenaywere

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <noscript><h2>necesita Javascript para ver esta página correctamente.</h2></noscript>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
